f6b231b33160dd4c1eb4d7cb6ec1e8b24dd4b9a5
ajithks98086 <ajithks98086@gmail.com>
2025-04-08 17:49:44 +0000

    Add fields to userModel.js.

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -7,20 +7,32 @@ const refreshAccessToken = async (req, res) => {
   }
 
   try {
-    jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, async (err, decoded) => {
-      if (err) return res.status(403).json({ error: "Invalid or expired refresh token" });
+    jwt.verify(
+      refreshToken,
+      process.env.JWT_REFRESH_SECRET,
+      async (err, decoded) => {
+        if (err)
+          return res
+            .status(403)
+            .json({ error: "Invalid or expired refresh token" });
 
-      const user = await User.findById(decoded.id);
-      if (!user) return res.status(404).json({ error: "User not found" });
+        const user = await User.findById(decoded.id);
+        if (!user) return res.status(404).json({ error: "User not found" });
 
-      const { accessToken, refreshToken: newRefreshToken } = generateTokens(user);
+        const { accessToken, refreshToken: newRefreshToken } =
+          generateTokens(user);
 
-      res.status(200).json({
-        accessToken,
-        refreshToken: newRefreshToken,
-      });
-    });
+        res.status(200).json({
+          accessToken,
+          refreshToken: newRefreshToken,
+        });
+      }
+    );
   } catch (error) {
-    res.status(500).json({ error: "Internal server error", details: error.message });
+    res
+      .status(500)
+      .json({ error: "Internal server error", details: error.message });
   }
 };
+
+exports.refreshAccessToken = refreshAccessToken;
--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -22,8 +22,21 @@ const userSchema = new mongoose.Schema(
       type: String, // Store the URL or file path of the profile photo
       default: "", // You can set a default profile photo if needed
     },
+    isLoggedIn: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    blockedUsers:[
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      }
+    ]
   },
 );
 
 // Mongoose auto-generates a unique `_id` field (which serves as id)
 module.exports = mongoose.model("User", userSchema);
+
+// blockedUsers: This is an array of ObjectIds, and those IDs should refer to documents in the User collection.
