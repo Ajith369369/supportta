3b2b10809c5d25c54c8d7aead28bd93b91c58d8c
ajithks98086 <ajithks98086@gmail.com>
2025-04-09 04:00:38 +0000

    Add routes.

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const {
+  addProduct,
+  updateProduct,
+  deleteProduct,
   getAllProductsAddedByAllUsers,
   getMyProducts,
 } = require("../controllers/productController");
+const authenticateUser = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.get("/products", getAllProductsAddedByAllUsers); // All products with filters
-router.get("/my-products", getMyProducts); // Only my products
+router.get("/products/add-product", authenticateUser, addProduct);
+router.get("/products/:id", authenticateUser, updateProduct);
+router.get("/products/:id", authenticateUser, deleteProduct);
+router.get(
+  "/products/get-all-products",
+  authenticateUser,
+  getAllProductsAddedByAllUsers
+); // All products with filters
+router.get("/products/get-my-products", authenticateUser, getMyProducts); // Only my products
 
 module.exports = router;
