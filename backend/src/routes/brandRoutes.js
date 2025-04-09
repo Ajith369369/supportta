7023114bf63fa47faa6fc99884d2e4f21cbf4143
ajithks98086 <ajithks98086@gmail.com>
2025-04-09 05:07:23 +0000

    Edit brandRoutes.js.

--- backend/src/routes/brandRoutes.js
+++ backend/src/routes/brandRoutes.js
@@ -4,7 +4,7 @@ const { addBrand, getAllBrands } = require("../controllers/brandController");
 
 const router = express.Router();
 
-router.post("/brands", addBrand); // Add brand (auth required)
-router.get("/brands", getAllBrands);           // Get all brands (public)
+router.post("/brands/add-brand", addBrand); // Add brand (auth required)
+router.get("/brands/get-all-brands", getAllBrands);           // Get all brands (public)
 
 module.exports = router;
