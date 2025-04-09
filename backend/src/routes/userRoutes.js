7504737558bd29778c2fc650a857caad8be474a2
ajithks98086 <ajithks98086@gmail.com>
2025-04-09 03:09:58 +0000

    Add paths in userRoutes.js.

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -2,11 +2,15 @@ const express = require("express");
 const {
   registerUser,
   loginUser,
+  updateUserProfile,
+  deleteUserProfile,
 } = require("../controllers/userController");
 
 const router = express.Router();
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);// Delete user by ID
+router.post("/users/register", registerUser);
+router.post("/users/login", loginUser); // Delete user by ID
+router.put("/users/:id", updateUserProfile);
+router.delete("/users/:id", deleteUserProfile);
 
 module.exports = router;
